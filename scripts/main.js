9dbacb2ba2fc75eddb5fba42c1e934b49dd31c16
WeeraphatPhimsan <42836912+WeeraphatPhimsan@users.noreply.github.com>
2018-10-09 19:29:04 +0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+	var wordList; 
+	var requestURL = "Vocab2.json";
+	var request = new XMLHttpRequest();
+	request.open('GET',requestURL);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function(){
+		var heroesList = request.response;
+		showHeroes(heroesList);
+	}
+	
+	function showHeroes(jsonobj){
+		wordList = jsonobj['Vocabulary'];
+	}
+	function search(){
+		var wordToSearch = document.querySelector('.searchBox');
+		var definition = document.querySelector('.box');
+		var check,word = "";
+		check = wordToSearch.value; 
+		for (var i = 0 ; i < wordList.length ; i++){
+		 word = wordList[i].vocab;
+			if(word === wordToSearch.value){
+ 		definition.innerHTML  = word + " :" + "<br>" + '<i>' +  wordList[i].meaning + '</i>'; 		  
+		   }
+			 
+		}
+	}
+	
+
